daa2d088b72a17876a09bb403d11d13d3a7b7ed1
github honganji <yujicavs@icloud.com>
2023-05-30 17:06:18 +0900

    fix contract

--- packages/contract/contracts/Lock.sol
+++ packages/contract/contracts/Lock.sol
@@ -11,6 +11,11 @@ contract Lock {
   event Withdrawal(uint amount, uint when);
 
   constructor(uint _unlockTime) payable {
+    require(
+      block.timestamp < _unlockTime,
+      'Unlock time should be in the future'
+    );
+
     unlockTime = _unlockTime;
     owner = payable(msg.sender);
   }
@@ -19,7 +24,10 @@ contract Lock {
     // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
     // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
+    require(block.timestamp >= unlockTime, "You can't withdraw yet");
     require(msg.sender == owner, "You aren't the owner");
+
+    emit Withdrawal(address(this).balance, block.timestamp);
 
     owner.transfer(address(this).balance);
   }
